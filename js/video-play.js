2fe5509681ffe88eea3d35b17519b6a76e504b20
misba9 <ahmedmisba405@gmail.com>
2025-03-07 05:29:30 +0530

    pages update

---
+++ js/video-play.js
@@ -0,0 +1,138 @@
+// video1
+  document.addEventListener('DOMContentLoaded', () => {
+        const videoContainer = document.querySelector('.video-box1');
+        const video = document.querySelector('.video1');
+        // const overlay = document.querySelector('.overlay');
+
+        function playVideo() {
+            video.play();
+            overlay.style.opacity = '0';
+        }
+
+        function pauseVideo() {
+            video.pause();
+            overlay.style.opacity = '1';
+        }
+
+        videoContainer.addEventListener('mouseenter', playVideo);
+        videoContainer.addEventListener('mouseleave', pauseVideo);
+
+        // Add touch support for mobile devices
+        videoContainer.addEventListener('touchstart', playVideo);
+        videoContainer.addEventListener('touchend', pauseVideo);
+    });
+    // video2
+  document.addEventListener('DOMContentLoaded', () => {
+        const videoContainer = document.querySelector('.video-box2');
+        const video = document.querySelector('.video2');
+        // const overlay = document.querySelector('.overlay');
+
+        function playVideo() {
+            video.play();
+            overlay.style.opacity = '0';
+        }
+
+        function pauseVideo() {
+            video.pause();
+            overlay.style.opacity = '1';
+        }
+
+        videoContainer.addEventListener('mouseenter', playVideo);
+        videoContainer.addEventListener('mouseleave', pauseVideo);
+
+        // Add touch support for mobile devices
+        videoContainer.addEventListener('touchstart', playVideo);
+        videoContainer.addEventListener('touchend', pauseVideo);
+    });
+    // video3
+  document.addEventListener('DOMContentLoaded', () => {
+        const videoContainer = document.querySelector('.video-box3');
+        const video = document.querySelector('.video3');
+        // const overlay = document.querySelector('.overlay');
+
+        function playVideo() {
+            video.play();
+            overlay.style.opacity = '0';
+        }
+
+        function pauseVideo() {
+            video.pause();
+            overlay.style.opacity = '1';
+        }
+
+        videoContainer.addEventListener('mouseenter', playVideo);
+        videoContainer.addEventListener('mouseleave', pauseVideo);
+
+        // Add touch support for mobile devices
+        videoContainer.addEventListener('touchstart', playVideo);
+        videoContainer.addEventListener('touchend', pauseVideo);
+    });
+    // video4
+  document.addEventListener('DOMContentLoaded', () => {
+        const videoContainer = document.querySelector('.video-box4');
+        const video = document.querySelector('.video4');
+        // const overlay = document.querySelector('.overlay');
+
+        function playVideo() {
+            video.play();
+            overlay.style.opacity = '0';
+        }
+
+        function pauseVideo() {
+            video.pause();
+            overlay.style.opacity = '1';
+        }
+
+        videoContainer.addEventListener('mouseenter', playVideo);
+        videoContainer.addEventListener('mouseleave', pauseVideo);
+
+        // Add touch support for mobile devices
+        videoContainer.addEventListener('touchstart', playVideo);
+        videoContainer.addEventListener('touchend', pauseVideo);
+    });
+    // video5
+  document.addEventListener('DOMContentLoaded', () => {
+        const videoContainer = document.querySelector('.video-box5');
+        const video = document.querySelector('.video5');
+        // const overlay = document.querySelector('.overlay');
+
+        function playVideo() {
+            video.play();
+            overlay.style.opacity = '0';
+        }
+
+        function pauseVideo() {
+            video.pause();
+            overlay.style.opacity = '1';
+        }
+
+        videoContainer.addEventListener('mouseenter', playVideo);
+        videoContainer.addEventListener('mouseleave', pauseVideo);
+
+        // Add touch support for mobile devices
+        videoContainer.addEventListener('touchstart', playVideo);
+        videoContainer.addEventListener('touchend', pauseVideo);
+    });
+    // video6
+  document.addEventListener('DOMContentLoaded', () => {
+        const videoContainer = document.querySelector('.video-box6');
+        const video = document.querySelector('.video6');
+        // const overlay = document.querySelector('.overlay');
+
+        function playVideo() {
+            video.play();
+            overlay.style.opacity = '0';
+        }
+
+        function pauseVideo() {
+            video.pause();
+            overlay.style.opacity = '1';
+        }
+
+        videoContainer.addEventListener('mouseenter', playVideo);
+        videoContainer.addEventListener('mouseleave', pauseVideo);
+
+        // Add touch support for mobile devices
+        videoContainer.addEventListener('touchstart', playVideo);
+        videoContainer.addEventListener('touchend', pauseVideo);
+    });
